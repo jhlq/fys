8e8aa8dc1bb425a2735e1cae93145bfef325ad62
qni <marcus.appelros@gmail.com>
2015-08-01 12:48:52 +0200

    <3

--- ann/heart/brain.jl
+++ ann/heart/brain.jl
@@ -1,13 +1,18 @@
+include("heart.jl")
+heart=drawheart!()
+
 numneurons=9
 net=rand(numneurons,numneurons)-0.5
 exc=zeros(numneurons) #excitations
 exc[1]=1
 
-w=12
-h=10
+w=120
+h=100
 canvas=zeros(h,w)
 
-loc=Integer[h-3,w/2]
+loc=Integer[w/2,h]
+
+#=
 
 function sigmoid(x)
 	1/(1+exp(-x))
@@ -18,23 +23,42 @@ function sigmoid(a::Array)
 	end
 	return a
 end
+nstep=999
+for st in 1:nstep
+	step!(net,exc,loc,canvas)
+end
+=#
 
 function step!(net,exc,loc,canvas)
 	exc[1:end]=atan(net*exc)
 	x=Integer(sign(exc[end-1]))
 	y=Integer(sign(exc[end]))
 	h,w=size(canvas)
-	loc[1]=mod(loc[1]+x,h)
-	loc[2]=mod(loc[2]+y,w)
-	canvas[loc[1]+1,loc[2]+1]+=1
+	loc[1]=mod(loc[1]+x,w)
+	loc[2]=mod(loc[2]+y,h)
+	canvas[loc[2]+1,loc[1]+1]+=1
 end
-
-nstep=999
-for st in 1:nstep
-	step!(net,exc,loc,canvas)
+rnet()=rand(9,9)-0.5
+function makedrawing(net=rnet(),steps=300)
+	h=100;w=120
+	canvas=zeros(h,w)
+	neurons=size(net,1)
+	exc=zeros(neurons) #excitations
+	exc[1]=1
+	loc=Integer[w/2,h]
+	for st in 1:steps
+		step!(net,exc,loc,canvas)
+	end
+	return canvas
 end
 
 #=
 using Winston
 imagesc(canvas)
 =#
+
+function save(net,filename)
+	sn=open(filename,"w") #write append
+	write(sn,"$net")
+	close(sn)
+end
---
+++ ann/heart/heart.jl
@@ -0,0 +1,50 @@
+function topol(x,y,o)
+
+end
+function frompol(r,th,o)
+	x=round(o[1]+r*cos(th))
+	y=round(o[2]+r*sin(th))
+	return [x,y]
+end
+function spiral(th)
+	return exp(th/2)
+end
+
+w=120
+h=100
+canvas=zeros(h,w)
+o=[w/2,h-h/5]
+
+#spiral->map to array of length 628-> assemble matrix: 
+#coord=[ox+r*cos(ind/100),oy+r*sin(ind/100)]
+#iterate over canvas and fill every point within a certain distance from coord
+function drawheart!(canvas=zeros(100,120))
+	h,w=size(canvas)
+	o=[w/2,h-h/5]
+	f=x->x
+	a=map(f,1:314)
+	a/=maximum(a)
+	r=h-h/4
+	spiralx=Any[o[1]]
+	spiraly=Any[o[2]]
+	for i in 1:length(a)
+		coord=[o[1]+r*a[i]*cos(i/100+pi/2),o[2]+r*a[i]*sin(i/100+pi/2)]
+		coordr=[o[1]+r*a[i]*cos(-i/100+pi/2),o[2]+r*a[i]*sin(-i/100+pi/2)]
+		push!(spiralx,coord[1],coordr[1])
+		push!(spiraly,coord[2],coordr[2])
+	end
+
+	distance(p1,p2)=sqrt((p1[1]-p2[1])^2+(p1[2]-p2[2])^2)
+	for i in 1:h
+		for j in 1:w
+			for poind in 1:length(spiralx)
+				if distance([j,i],[spiralx[poind],spiraly[poind]])<5
+					canvas[h-i+1,j]=1
+				end
+			end
+		end
+	end
+	return canvas
+end
+
+#canvas->iterate each point
