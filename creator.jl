3ec4a0a62c80e1a7afa55d82a7599cc0ec335a8e
Marcus Appelros <marcus.appelros@gmail.com>
2016-01-22 20:56:33 +0300

    Update creator.jl

--- creator.jl
+++ creator.jl
@@ -1,10 +1,14 @@
 using HttpServer
 
 http = HttpHandler() do req::Request, res::Response
+	println(UTF8String(req.data))
 	if ismatch(r"^/create/",req.resource)
+		t=URIParser.unescape(split(req.resource,'/')[3])
+		println(UTF8String(t))
+		txt=UTF8String(t)
 		f=open("profile.txt","w")
-		txt=URIParser.unescape(split(req.resource,'/')[3])
 		write(f,txt)
+		close(f)
 		return Response("Wrote in profile.txt: $txt")
 	else
 		Response(404)
