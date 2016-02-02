fd5f39a92cab7a000b0729777d07884fcfab141c
Marcus Appelros <marcus.appelros@gmail.com>
2016-02-02 13:31:54 +0100

    Update welcome.php

--- GaC/welcome.php
+++ GaC/welcome.php
@@ -4,17 +4,40 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
 
-<?php 
-#setup db in sqlite3 with: create table people(name text, email text);
-include_once 'medoo.php';
-$database = new medoo([
-	'database_type' => 'sqlite',
-	'database_file' => 'data/emails.db'
-]);
-$arr=["name" => $_POST["name"],"email"=>$_POST["email"]];
-echo $arr["name"];
-$q=$database->insert("people", $arr);
-echo $q;
+<?php
+  try
+  {
+    //open the database
+    $db = new PDO('sqlite:dogsDb_PDO.sqlite');
+
+    //create the database
+    $db->exec("CREATE TABLE Dogs (Id INTEGER PRIMARY KEY, Breed TEXT, Name TEXT, Age INTEGER)");    
+
+    //insert some data...
+    $db->exec("INSERT INTO Dogs (Breed, Name, Age) VALUES ('Labrador', 'Tank', 2);".
+               "INSERT INTO Dogs (Breed, Name, Age) VALUES ('Husky', 'Glacier', 7); " .
+               "INSERT INTO Dogs (Breed, Name, Age) VALUES ('Golden-Doodle', 'Ellie', 4);");
+
+    //now output the data to a simple html table...
+    print "<table border=1>";
+    print "<tr><td>Id</td><td>Breed</td><td>Name</td><td>Age</td></tr>";
+    $result = $db->query('SELECT * FROM Dogs');
+    foreach($result as $row)
+    {
+      print "<tr><td>".$row['Id']."</td>";
+      print "<td>".$row['Breed']."</td>";
+      print "<td>".$row['Name']."</td>";
+      print "<td>".$row['Age']."</td></tr>";
+    }
+    print "</table>";
+
+    // close the database connection
+    $db = NULL;
+  }
+  catch(PDOException $e)
+  {
+    print 'Exception : '.$e->getMessage();
+  }
 ?>
 
 </body>
